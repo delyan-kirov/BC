09e2bc46f5f44d75e95eb145887fe49b56aa2e6a
delyan-kirov <delyan.k.kirov@gmail.com>
2026-02-22 11:26:56 +0200

    Cleaned test-suite

--- tst/tst_addition_n_subtraction.cpp
+++ tst/tst_addition_n_subtraction.cpp
@@ -1,15 +1,34 @@
 #include "TL.hpp"
 #include "UT.hpp"
+#include <cstdio>
 
-constexpr UT::String sut_file       = "./dat/addition.se";
-constexpr UT::String sut_file_basic = "./dat/basic.se";
+constexpr UT::String sut_file        = "./dat/addition.se";
+constexpr UT::String sut_file_basic  = "./dat/basic.se";
+constexpr UT::String sut_file_raylib = "./dat/raylib.se";
+
+constexpr bool RUN        = false;
+constexpr bool RUN_BASIC  = false;
+constexpr bool RUN_RAYLIB = true;
 
 int
 main()
 {
   AR::Arena arena{};
-  (void)sut_file_basic;
-  (void)sut_file;
 
-  TL::Mod mod(sut_file_basic, arena);
+  if (RUN)
+  {
+    TL::Mod mod(sut_file, arena);
+  }
+  else if (RUN_BASIC)
+  {
+    TL::Mod mod_basic(sut_file_basic, arena);
+  }
+  else if (RUN_RAYLIB)
+  {
+    TL::Mod mod_raylib(sut_file_raylib, arena);
+  }
+  else
+  {
+    std::printf("tst_addition_n_subtraction -> OK [no target ran]\n");
+  }
 }
